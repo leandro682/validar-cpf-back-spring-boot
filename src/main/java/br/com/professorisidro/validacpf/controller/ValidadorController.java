9d12e8322e0a8c2e73dd58e162b1b7042cfa8e73
LEANDRO GUIMARAES DA SILVA <987278140@itaud.des.ihf>
2020-08-24 15:26:07 -0300

    versionando home action

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -18,5 +18,8 @@ public class ValidadorController {
 		}
 		 return "{\"status\": \"invalido\"}";
 	}
-
+	@GetMapping("/")
+	public String index() {
+			 return "{\"mensagem\": \"API Itau Turma 7C\"}";
+	}
 }
